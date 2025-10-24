2928eccc2040a3da4869558b5d2a0d839490173c
CarlosMarrero98 <alu0101210995@ull.edu.es>
2025-10-24 02:23:53 +0100

    feat: add basic grid layout and buttons

--- App.tsx
+++ App.tsx
@@ -4,8 +4,29 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tres en Linea</Text>
+
+      <View style={styles.board}>
+
+        <View style={styles.row}>
+          <Pressable style={styles.button}><Text style={styles.title}>1</Text></Pressable>
+          <Pressable style={styles.button}><Text style={styles.title}>2</Text></Pressable>
+          <Pressable style={styles.button}><Text style={styles.title}>3</Text></Pressable>
+        </View>
+
+        <View style={styles.row}>
+          <Pressable style={styles.button}><Text style={styles.title}>4</Text></Pressable>
+          <Pressable style={styles.button}><Text style={styles.title}>5</Text></Pressable>
+          <Pressable style={styles.button}><Text style={styles.title}>6</Text></Pressable>
+        </View>
+
+        <View style={styles.row}>
+          <Pressable style={styles.button}><Text style={styles.title}>7</Text></Pressable>
+          <Pressable style={styles.button}><Text style={styles.title}>8</Text></Pressable>
+          <Pressable style={styles.button}><Text style={styles.title}>9</Text></Pressable>
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -17,4 +38,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  row: {
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+
+  board: {
+    width: 375,
+    height: 375,
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#333",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    width: 125,
+    height: 125,
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#333",
+  }
 });
